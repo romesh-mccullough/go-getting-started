3d68c94ef98794085642034293ee10e8090fffa3
Jon Mountjoy <tech@jonmountjoy.com>
2015-06-30 12:40:37 +0100

    add config var and dependency example

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -4,16 +4,40 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"bytes"
+	"strconv"
 
 	"github.com/heroku/go-getting-started/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"github.com/heroku/go-getting-started/Godeps/_workspace/src/github.com/russross/blackfriday"
 )
 
+var (
+    repeat int
+)
+
+func repeatFunc(c *gin.Context) {
+	var buffer bytes.Buffer
+	for i := 0; i < repeat; i++ {
+        buffer.WriteString("Hello from Go!")
+  }
+	c.String(http.StatusOK, buffer.String())
+}
+
+
 func main() {
+	var err error
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+
+  tStr := os.Getenv("REPEAT")
+  repeat, err = strconv.Atoi(tStr)
+  if err != nil {
+    log.Print("Error converting $REPEAT to an int: %q - Using default", err)
+		repeat = 5
+  }
 
 	router := gin.New()
 	router.Use(gin.Logger())
@@ -23,6 +47,12 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
+	router.GET("/mark", func(c *gin.Context) {
+		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+	})
+
+	router.GET("/repeat", repeatFunc)
 
 	router.Run(":" + port)
 }
